1a1895f1718a83048ec8b918b880f1439af321a5
zhwuba <zhwuba@yahoo.com>
2015-10-14 18:28:33 +0800

    for loginActivity

--- app/src/main/java/com/mimi/github/accounts/LoginActivity.java
+++ app/src/main/java/com/mimi/github/accounts/LoginActivity.java
@@ -1,7 +1,13 @@
 package com.mimi.github.accounts;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.github.kevinsawicki.wishlist.ViewFinder;
 import com.mimi.github.R;
 import com.mimi.github.roboactivities.RoboActionBarAccountAuthenticatorActivity;
 
@@ -12,10 +18,25 @@ public class LoginActivity extends RoboActionBarAccountAuthenticatorActivity{
 
     public static final String PARAM_AUTHTOKEN_TYPE = "authtokenType";
 
+    private AutoCompleteTextView loginText;
+
+    private EditText passwordText;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.login);
+
+        ViewFinder viewFinder = new ViewFinder(this);
+        loginText = viewFinder.find(R.id.et_login);
+        passwordText = viewFinder.find(R.id.et_password);
+
+
+        TextView signupText = viewFinder.find(R.id.tv_signup);
+        signupText.setMovementMethod(LinkMovementMethod.getInstance());
+        signupText.setText(Html.fromHtml(getString(R.string.signup_link)));
+
+
     }
 }
